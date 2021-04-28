0afc1951d7ba11adb1cae50acddcdfd12f019819
Andrés Torres <atorres@medialima.mx>
2021-04-28 08:49:29 -0500

    Prueba Correcta

--- src/test/java/com/example/democi/PruebaServiceUnitTest.java
+++ src/test/java/com/example/democi/PruebaServiceUnitTest.java
@@ -12,6 +12,6 @@ public class PruebaServiceUnitTest extends DemoCiApplicationTests{
 
     @Test
     public void sumaTest(){
-        assertEquals(5.0,pruebaService.suma(2.0,5.0));
+        assertEquals(7.0,pruebaService.suma(2.0,5.0));
     }
 }
